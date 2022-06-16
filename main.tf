da7d0ee2eee8f1f1c6f0e6f93bc9af03e733dbdd
EC2 Default User <ec2-user@ip-172-31-85-174.ec2.internal>
2022-06-16 16:05:49 +0000

    point backend to correct TFC organization and workspace

--- main.tf
+++ main.tf
@@ -12,10 +12,10 @@ terraform {
   required_version = "~> 1.0"
 
   backend "remote" {
-    organization = "REPLACE_ME"
+    organization = "DL-Terraform-Lab"
 
     workspaces {
-      name = "REPLACE_ME"
+      name = "gh-actions"
     }
   }
 }
